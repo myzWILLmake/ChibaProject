cc370a340763805d528ea80eab31d452abf96be3
Yunze <yunze.moe@outlook.com>
2016-04-10 17:19:07 +0800

    modify Login code to use internet connection

--- app/src/main/java/moe/akagi/chibaproject/activity/Login.java
+++ app/src/main/java/moe/akagi/chibaproject/activity/Login.java
@@ -5,14 +5,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Map;
+
 import moe.akagi.chibaproject.MyApplication;
 import moe.akagi.chibaproject.R;
-import moe.akagi.chibaproject.database.API;
 import moe.akagi.chibaproject.datatype.User;
+import moe.akagi.chibaproject.network.API;
+import moe.akagi.chibaproject.network.RetrieveData.DataListener;
+import moe.akagi.chibaproject.network.RetrieveData.Manager;
+//import moe.akagi.chibaproject.database.API;
+
 
 /**
  * Created by yunze on 12/5/15.
@@ -51,11 +58,23 @@ public class Login extends Activity {
         ActivityCollector.removeActivity(this);
     }
 
-    private void submitLogin(String phone, String password) {
-        User user = API.getUserByAuth(phone, password);
-        if (user == null) {
-            Toast.makeText(this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
-        } else {
+    private void submitLogin(final String phone, final String password) {
+        Manager manager = new Manager();
+        manager.addDataListener(new DataListener() {
+            @Override
+            public void onDataReady(Object data) {
+                handleLogin(data, phone, password);
+            }
+        });
+        API.submitLogin(manager, phone, password);
+    }
+
+    private void handleLogin(Object data, String phone, String password) {
+        if (data instanceof User) {
+            User user = (User)data;
+            Log.v("login", user.get_id());
+            Log.v("login", user.getNickname());
+            Log.v("login", user.getPhone());
             MyApplication.user = user;
             SharedPreferences.Editor editor = getSharedPreferences("AppData", MODE_PRIVATE).edit();
             editor.putBoolean("logged", true);
@@ -66,8 +85,24 @@ public class Login extends Activity {
             intent.putExtra("login_succeed", true);
             setResult(RESULT_OK, intent);
             ActivityCollector.removeActivity(this);
+        } else {
+            int state = (Integer)data;
+            switch (state) {
+                case 101:
+                    Toast.makeText(this, "已经登陆了啊?", Toast.LENGTH_SHORT).show();
+                    break;
+                case 102:
+                    Toast.makeText(this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                    break;
+                case 110:
+                    Toast.makeText(this, "有些不对劲!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(this, "返回了未知的状态", Toast.LENGTH_SHORT).show();
+            }
         }
     }
+
 
     @Override
     public void onBackPressed() {
