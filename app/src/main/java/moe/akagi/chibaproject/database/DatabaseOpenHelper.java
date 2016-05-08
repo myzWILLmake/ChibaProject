cdce16a28983135488c58b961790db512a68dc8f
Yunze <yunze.moe@outlook.com>
2016-05-08 23:28:55 +0800

    fix typo

--- app/src/main/java/moe/akagi/chibaproject/database/DatabaseOpenHelper.java
+++ app/src/main/java/moe/akagi/chibaproject/database/DatabaseOpenHelper.java
@@ -40,7 +40,7 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
     private static final String CREATE_PARTIN = "CREATE TABLE part_in (" +
             "id         integer PRIMARY KEY AUTOINCREMENT," +
             "event_id   integer NOT NULL," +
-            "usr_id    integer NOU NULL)";
+            "usr_id    integer NOT NULL)";
 
     private static final String CREATE_LAUNCH = "CREATE TABLE launch (" +
             "id         integer PRIMARY KEY AUTOINCREMENT," +
