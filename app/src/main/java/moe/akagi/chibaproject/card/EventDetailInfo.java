ba02280c0ae11f22e668d59c3ca6d6d16882a66b
15cm <sinkerine@gmail.com>
2016-05-10 00:18:35 +0800

    Bind click event on '显示地图'

--- app/src/main/java/moe/akagi/chibaproject/card/EventDetailInfo.java
+++ app/src/main/java/moe/akagi/chibaproject/card/EventDetailInfo.java
@@ -1,20 +1,36 @@
 package moe.akagi.chibaproject.card;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import moe.akagi.chibaproject.R;
+import moe.akagi.chibaproject.activity.PlaceDisplay;
 import moe.akagi.chibaproject.datatype.Event;
 
 /**
  * Created by a15 on 12/11/15.
  */
 public class EventDetailInfo extends EventInfo {
+
     public EventDetailInfo(Context context, Event event) {
         super(context, R.layout.event_detail_info_content, event);
         this.addCardHeader(null);
         this.imageViewId = R.id.event_detail_info_image;
         this.timeViewId = R.id.event_detail_info_time;
         this.placeViewId = R.id.event_detail_info_place;
+    }
+
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+        super.setupInnerViewElements(parent, view);
+        showMapText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlaceDisplay.actionStart(v.getContext(),location);
+            }
+        });
     }
 }
